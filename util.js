aee0ffb528f1fd51f17098d537e107a7fff4b0b7
courage <277329670@qq.com>
2023-05-31 13:50:03 +0800

    lid-77777

--- util.js
+++ util.js
@@ -7,11 +7,10 @@ function foo() {
 
 console.log("ok");
 
-<<<<<<< HEAD
 console.log(444);
 55555;
 console.log(6666);
-=======
-55555
-666-remote
->>>>>>> c6396ec9b50d68e8cb7b0aa5f3429fcf9491f2d8
+55555;
+666 - remote;
+
+777777777777 - lid;
